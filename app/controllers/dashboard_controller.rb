34aed2f4d91ad49b74d04a2a0ddc2627e9d314fd
egreerdp <ewan.greer@dailypay.com>
2025-04-29 22:59:44 +0100

    feat: add events to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   def index
     @user = Current.user
     @reservations = Reservation.where(user_id: @user.id)
+    @events = Event.all
   end
 end
